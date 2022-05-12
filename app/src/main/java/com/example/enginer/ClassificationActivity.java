b459a1f3f31202c6c5b35ed8f94e1ece31a639d7
lcaprioli96 <93045436+lcaprioli17@users.noreply.github.com>
2022-05-12 11:43:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/enginer/ClassificationActivity.java
@@ -0,0 +1,75 @@
+package com.example.enginer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioRecord;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.tensorflow.lite.support.audio.TensorAudio;
+import org.tensorflow.lite.support.label.Category;
+import org.tensorflow.lite.task.audio.classifier.AudioClassifier;
+import org.tensorflow.lite.task.audio.classifier.Classifications;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class ClassificationActivity extends AppCompatActivity {
+
+    Float probabilityThreshold = 0.3f;
+    TextView textView;
+    String path= "car_model.tflite";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classification);
+
+        textView = findViewById(R.id.output);
+        TextView recorderSpecsTextView = findViewById(R.id.textViewAudioRecorderSpecs);
+        try {
+            AudioClassifier classifier= AudioClassifier.createFromFile(this,path );
+            TensorAudio tensor = classifier.createInputTensorAudio();
+            TensorAudio.TensorAudioFormat format = classifier.getRequiredTensorAudioFormat();
+            String recorderSpecs = "Number Of Channels: " +  format.getChannels() + "\n" +
+                    "Sample Rate: " + format.getSampleRate();
+            recorderSpecsTextView.setText(recorderSpecs);
+            AudioRecord record = classifier.createAudioRecord();
+            record.startRecording();
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    tensor.load(record);
+                    List<Classifications> output = classifier.classify(tensor);
+
+                    Category category1 = output.get(0).getCategories().get(0);
+                    Category category2 = output.get(1).getCategories().get(0);
+
+                    String outputStr;
+
+                    if (category1.getLabel().equals("Vehicle")) {
+                        outputStr = category1.getLabel() + ": " + category2.getLabel() + ": " + category2.getScore() + "\n";
+                        textView.setText(outputStr);
+                    }else{
+                        textView.setText("No car found.");
+                    }
+
+                    /*List<Category> finalOutput1 = categories;
+                    runOnUiThread(() -> {
+                        if (finalOutput1.isEmpty())
+                            textView.setText("Could not identify the car");
+                        else
+                            textView.setText(outputStr.toString());*/
+
+                    }
+            };
+            Timer t = new Timer();
+            t.schedule(task, 1, 500);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/enginer/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.enginer;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioRecord;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.Manifest.permission.CAPTURE_AUDIO_OUTPUT;
+import static android.Manifest.permission.RECORD_AUDIO;
+
+import org.tensorflow.lite.support.audio.TensorAudio;
+import org.tensorflow.lite.task.audio.classifier.AudioClassifier;
+import org.tensorflow.lite.task.audio.classifier.Classifications;
+import org.tensorflow.lite.task.core.BaseOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+//Toast.makeText(MainActivity.this,"Permission Denied.Closing the app...",Toast.LENGTH_SHORT).show();
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private boolean permissionToRecordAccepted = false;
+
+    //Audio recording permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        }
+        if (!permissionToRecordAccepted ) finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+    }
+
+
+    //Start recording button
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void startRec(View view) {
+
+        Intent i = new Intent(this, ClassificationActivity.class);
+        startActivity(i);
+
+    }
+}
